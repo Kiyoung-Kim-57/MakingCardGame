15b97d4bb083171aa8ad65a88cae04a5d7e5f145
Kiyoung <rlaky45@naver.com>
2023-08-31 11:45:31 +0900

    [Chore] 폴더링

---
+++ MakingCardGame/MakingCardGame/VIew/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  MakingCardGame
+//
+//  Created by 김기영 on 2023/08/31.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Text("dd")
+        }
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
